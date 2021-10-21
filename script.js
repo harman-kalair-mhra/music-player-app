80abcfd5a9e988e6225694943ac8538cc48ba6e7
harmankalair123 <harman.kalair@ada.ac.uk>
2021-10-21 23:21:09 +0100

    started to define variables

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+// Importing all elements from the HTML page
+// assigning using appropriate variable names
+let now_playing = document.querySelector(".now-playing")
+let track_art = document.querySelector(".track-art")
+let track_name = document.querySelector(".track-name")
+let track_artist = document.querySelector(".track-artist")
