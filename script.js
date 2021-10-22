83dcdd00ab3b6a316d0e3f267b6bff05c5441d9a
harmankalair123 <harman.kalair@ada.ac.uk>
2021-10-22 11:19:46 +0100

    defined all variables

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ let now_playing = document.querySelector(".now-playing")
 let track_art = document.querySelector(".track-art")
 let track_name = document.querySelector(".track-name")
 let track_artist = document.querySelector(".track-artist")
+
+let playpause_btn = document.querySelector(".playpause-track")
+let next_btn = document.querySelector(".next-track")
+let previous_btn = document.querySelector("prev-track")
+
+let seek_slider = document.querySelector(".seek_slider")
+let volume_slider = document.querySelector(".volume_slider")
+let current_time = document.querySelector(".current-time")
+let total_duration = document.querySelector(".total-duration")
+
+//Declaring global variables
+let track_index = 0
+let isPlaying = false
+let updateTimer
+
+//audio element
+let current_track = document.createElement('audio')
